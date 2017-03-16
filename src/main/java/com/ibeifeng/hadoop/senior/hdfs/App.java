0bfa99dd5b0ea2b4b1aaac8e1736179656e55b66
lihu341228 <945763695@qq.com>
2017-03-16 15:58:48 +0800

    erAHb4w 5t6y4

---
+++ src/main/java/com/ibeifeng/hadoop/senior/hdfs/App.java
@@ -0,0 +1,11 @@
+package com.ibeifeng.hadoop.senior.hdfs;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
